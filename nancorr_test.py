78234d18ef32b71de69f74a2f48be5cf61f27d6e
Oleksandr Frei <oleksandr.frei@gmail.com>
2023-11-06 18:43:01 +0100

    nancorr_test.py

---
+++ nancorr_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import nancorr
+A = np.random.rand(10, 3)
+B = np.random.rand(10, 2)
+
+A[1, 1] = np.nan
+
+r,t,n = nancorr.nancorr(A, B)
+
+for i in range(A.shape[1]):
+    for j in range(B.shape[1]):
+        idx = np.isfinite(A[:, i] + B[:, j])
+        assert np.isclose(r[i, j], (np.corrcoef(A[idx, i], B[idx, j])[0, 1]))
+print('ok')
